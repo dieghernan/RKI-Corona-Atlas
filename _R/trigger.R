e560a7a2be7ead53bdbf0d5ee7cff770927ad8e2
dieghernan <diego.hernangomezherrero@gmail.com>
2021-04-21 22:46:56 +0200

    Revamp and add searches

--- _R/trigger.R
+++ _R/trigger.R
@@ -1,2 +1,9 @@
 rmarkdown::render("_R/test.Rmd", output_dir = "_pages/",
                   output_format = "md_document", quiet = TRUE)
+#
+# rmarkdown::render("_R/nuts.Rmd", output_dir = "_pages/",
+#                   output_format = "md_document", quiet = TRUE)
+#
+# rmarkdown::render("_R/lau.Rmd", output_dir = "_pages/",
+#                   output_format = "md_document", quiet = TRUE)
+
