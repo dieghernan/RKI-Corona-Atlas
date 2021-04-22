0931d2965121d0638935538cd5f976fa371b1727
dieghernan <diego.hernangomezherrero@gmail.com>
2021-04-22 11:32:37 +0000

    Start working on Kosovo #1

---
+++ _R/download_shape.R
@@ -0,0 +1,23 @@
+library(sf)
+library(dplyr)
+
+tmp <- tempfile(fileext = ".geojson")
+download.file("https://nominatim.openstreetmap.org/search?q=Kosovo&format=geojson&polygon_geojson=1",
+              tmp)
+
+r_shp <- st_read(tmp) %>% st_transform(4326)
+r_shp <- r_shp[1, ]
+
+df <- data.frame(ISO3_CODE = "XKX",
+                 NAME_ENGL = "Kosovo",
+                 stringsAsFactors = FALSE)
+
+final_shape <- st_sf(df, geometry=st_geometry(r_shp)) %>% st_make_valid()
+
+plot(st_geometry(final_shape))
+
+st_write(final_shape,"assets/data/geojson/kosovo.geojson")
+rm(list=ls())
+
+b <- st_read("assets/data/geojson/kosovo.geojson")
+plot(st_geometry(b))
