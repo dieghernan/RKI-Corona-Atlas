55f9454200793383d6878705290ad7461beffad8
dieghernan <diego.hernangomezherrero@gmail.com>
2021-05-01 15:25:30 +0200

    Using messages

--- _R/trigger.R
+++ _R/trigger.R
@@ -1,12 +1,17 @@
+
+message("Render index_en")
 rmarkdown::render("_R/index_en.Rmd", output_dir = "_pages/",
                   output_format = "md_document", quiet = TRUE)
 
+message("Render index")
 rmarkdown::render("_R/index.Rmd", output_dir = "_pages/",
                   output_format = "md_document", quiet = TRUE)
 
+message("Render heatmap")
 rmarkdown::render("_R/labs_heatmap.Rmd", output_dir = "_pages/",
                   output_format = "md_document", quiet = TRUE)
 
+message("Render og_image")
 # Dinamic og image
 source("_R/og_image.R")
 
