0f45d98edcf3a7af0691f40ee1c6f609e9f09756
Rodrigo <rodrigoherher@gmail.com>
2021-05-01 15:05:47 +0200

    Update trigger.R

--- _R/trigger.R
+++ _R/trigger.R
@@ -7,12 +7,6 @@ rmarkdown::render("_R/index.Rmd", output_dir = "_pages/",
 rmarkdown::render("_R/labs_heatmap.Rmd", output_dir = "_pages/",
                   output_format = "md_document", quiet = TRUE)
 
-rmarkdown::render("_R/labs_modular.Rmd", output_dir = "_pages/",
-                  output_format = "md_document", quiet = TRUE)
-
-rmarkdown::render("_R/labs_modular_de.Rmd", output_dir = "_pages/",
-                  output_format = "md_document", quiet = TRUE)
-
 # Dinamic og image
 source("_R/og_image.R")
 
