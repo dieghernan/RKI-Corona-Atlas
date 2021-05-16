4ce784be44b70e388a34f1befa205df167c7985c
Rodrigo Hernangómez <rodrigoherher@gmail.com>
2021-05-16 22:53:56 +0200

    Integrate French sources

--- _R/trigger.R
+++ _R/trigger.R
@@ -24,6 +24,14 @@ rmarkdown::render(
   quiet = TRUE
 )
 
+message("----Render index_fr\n\n")
+rmarkdown::render(
+  "_R/index_fr.Rmd",
+  output_dir = "_pages/",
+  output_format = "md_document",
+  quiet = TRUE
+)
+
 message("----Render heatmap\n\n")
 rmarkdown::render(
   "_R/labs_heatmap.Rmd",
