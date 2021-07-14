32eaa528b0eff26753413abbbb78896b33e996bd
Diego H <diego.hernangomezherrero@gmail.com>
2021-07-14 08:29:10 +0200

    Trigger

--- _R/download_shape.R
+++ _R/download_shape.R
@@ -2,7 +2,6 @@ library(sf)
 library(dplyr)
 
 
-
 tmp <- tempfile(fileext = ".geojson")
 download.file("https://nominatim.openstreetmap.org/search?q=Kosovo&format=geojson&polygon_geojson=1",
               tmp)
